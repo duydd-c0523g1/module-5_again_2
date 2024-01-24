25fc1492c5b544fd256df6a2d0853d64499b635f
duydd-c0623g1 <daoducduy.work@gmail.com>
2024-01-24 13:58:05 +0700

    commit

---
+++ ss02-01/src/App.js
@@ -0,0 +1,15 @@
+import Students from "./components/Students";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Students />
+      <Footer/>
+    </>
+  );
+}
+
+export default App;
---
+++ ss02-01/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+    return(
+        <footer>
+            <p>&copy; My student list</p>
+        </footer>
+    );
+}
+
+export default Footer
