80344880fed4eec5443a2c6e6f0dd151723f1ade
duydd-c0623g1 <daoducduy.work@gmail.com>
2024-01-24 14:06:09 +0700

    commit

---
+++ ss02/src/App.js
@@ -0,0 +1,15 @@
+import Students from "./components/ex1/Students";
+import Header from "./components/ex1/Header";
+import Footer from "./components/ex1/Footer";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Students />
+      <Footer/>
+    </>
+  );
+}
+
+export default App;
