95b70194b657ce4db3f19fb248ff523f8f68f5d5
duydd-c0623g1 <daoducduy.work@gmail.com>
2024-01-30 16:37:19 +0700

    commit

--- ss06/src/components/Books.jsx
+++ ss06/src/components/Books.jsx
@@ -1,15 +1,22 @@
 import { useState, useEffect } from "react";
 import { getAll } from "../service/BookService";
 import { findByName } from "../service/BookService";
-import {Link, NavLink} from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
+import { removeBook } from "../service/BookService";
 import "./style.css";
 
 export default function BookList() {
   const [bookList, setBookList] = useState([]);
+  const [bookDelete, setBookDelete] = useState({});
   const getAllBooks = async () => {
     let temp = await getAll();
     console.log(temp);
     setBookList(temp);
+  };
+
+  const deleteBook = async () => {
+    await removeBook(bookDelete);
+    getAllBooks();
   };
   useEffect(() => {
     getAllBooks();
@@ -33,7 +40,7 @@ export default function BookList() {
               <tbody>
                 {bookList.map((book, index) => (
                   <tr key={book.id}>
-                    <td>{index +1}</td>
+                    <td>{index + 1}</td>
                     <td>{book.title}</td>
                     <td>{book.quantity}</td>
                     <td>
@@ -45,7 +52,13 @@ export default function BookList() {
                         <button type="button" className="btn btn-warning">
                           Edit
                         </button>
-                        <button type="button" className="btn btn-danger">
+                        <button
+                          type="button"
+                          className="btn btn-danger"
+                          data-bs-toggle="modal"
+                          data-bs-target="#staticBackdrop"
+                          onClick={() => setBookDelete(book)}
+                        >
                           Delete
                         </button>
                       </div>
@@ -54,6 +67,50 @@ export default function BookList() {
                 ))}
               </tbody>
             </table>
+          </div>
+        </div>
+      </div>
+
+      <div
+        className="modal fade"
+        id="staticBackdrop"
+        data-bs-backdrop="static"
+        data-bs-keyboard="false"
+        tabIndex={-1}
+        aria-labelledby="staticBackdropLabel"
+        aria-hidden="true"
+      >
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h1 className="modal-title fs-5" id="staticBackdropLabel">
+                Delete book
+              </h1>
+              <button
+                type="button"
+                className="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              />
+            </div>
+            <div className="modal-body">Delete {bookDelete.title}?</div>
+            <div className="modal-footer">
+              <button
+                type="button"
+                className="btn btn-secondary"
+                data-bs-dismiss="modal"
+              >
+                Close
+              </button>
+              <button
+                type="button"
+                className="btn btn-danger"
+                data-bs-dismiss="modal"
+                onClick={deleteBook}
+              >
+                Confirm
+              </button>
+            </div>
           </div>
         </div>
       </div>
--- ss06/src/service/BookService.js
+++ ss06/src/service/BookService.js
@@ -1,14 +1,18 @@
 import axios from "axios";
 export const getAll = async () => {
-    let temp  = await axios.get('https://my-json-server.typicode.com/codegym-vn/mock-api-books/books')
+    let temp  = await axios.get('http://localhost:8080/books')
     return temp.data;
 }
 
 export const findByName = async (name) => {
-    let temp  = await axios.get('https://my-json-server.typicode.com/codegym-vn/mock-api-books/books?title_like=' + name)
+    let temp  = await axios.get('http://localhost:8080/books?title_like=' + name)
     return temp.data;
 }
 
 export const addBook = async (book) => {
-    await axios.post('https://my-json-server.typicode.com/codegym-vn/mock-api-books/books', book)
+    await axios.post('http://localhost:8080/books', book)
+}
+
+export const removeBook = async (book) => {
+    await axios.delete('http://localhost:8080/books/' + book.id)
 }
